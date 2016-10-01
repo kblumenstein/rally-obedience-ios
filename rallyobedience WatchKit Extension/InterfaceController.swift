491ae89c04a3a9a62121c26017335204fdfe7c17
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2016-10-01 16:47:20 +0200

    Initial Commit

---
+++ rallyobedience WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  rallyobedience WatchKit Extension
+//
+//  Created by Kerstin Blumenstein on 01/10/2016.
+//  Copyright © 2016 St. Poelten University of Applied Sciences. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ rallyobedience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rallyobedience
+//
+//  Created by Kerstin Blumenstein on 01/10/2016.
+//  Copyright © 2016 St. Poelten University of Applied Sciences. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
